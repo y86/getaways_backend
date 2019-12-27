0995f8442afff4d6e21362ab5f9ad2c498284660
y86 <yuri.de.figueiredo@gmail.com>
2019-12-27 19:17:06 -0300

    Include first commit

---
+++ lib/getaways_web/router.ex
@@ -0,0 +1,19 @@
+defmodule GetawaysWeb.Router do
+  use GetawaysWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+    plug GetawaysWeb.Plugs.SetCurrentUser
+  end
+
+  scope "/" do
+    pipe_through :api
+
+    forward "/api", Absinthe.Plug,
+      schema: GetawaysWeb.Schema.Schema
+
+    forward "/graphiql", Absinthe.Plug.GraphiQL,
+      schema: GetawaysWeb.Schema.Schema,
+      socket: GetawaysWeb.UserSocket
+    end
+end
